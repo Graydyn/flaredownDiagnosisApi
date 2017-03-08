c5fe92bdaaf3cffae17339108091539566132a4b
Graydyn <graydyn.young@gmail.com>
2017-03-08 14:39:05 -0500

    updated server port

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ class Generic(Resource):
 
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0',port=5000)
+    app.run(debug=False,host='0.0.0.0',port=80)
     #app.run(debug=True)
